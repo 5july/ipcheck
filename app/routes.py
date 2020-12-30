7d2743d747076f3c04e8fb24e82ed59f3b9d5f36
markus <markus@netalia.se>
2020-12-30 20:20:16 +0100

    add geoip info

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, jsonify
 from operator import itemgetter
 import re
 import redis
+import geoip2.database
 
 @app.route('/')
 def index_page():
@@ -32,7 +33,17 @@ def dnsleak(dns):
                     r.expire(dns, 15)
 
     new_list = []
+    reader = geoip2.database.Reader('extra/GeoLite2-ASN_20201229/GeoLite2-ASN.mmdb')
+    asn_no = None
+    asn_orgname = None
     for _x in r.lrange(dns, 0, 99):
-        new_list.append(_x.decode())
+        response = reader.asn(ip)
+        asn_no = response.autonomous_system_number
+        asn_orgname = response.autonomous_system_organization
+        data = {
+                "ip": _x.decode(),
+                "isp": asn_orgname
+                }
+        new_list.append(data)
 
     return jsonify({"dns": dns, "resolvers": new_list})
